3308f0587ce33fdcbd4cb837ecbd9c194701276f
Matt Pahuta <mhpahuta@gmail.com>
2023-04-24 11:14:00 -0700

    Add display note functionality, update placeholder icon w/font-awesome

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ class App {
     this.notes = [];
 
     // prepend vars with $ to quickly identify html elements over data
+    this.$placeholder = document.querySelector('#placeholder');
     this.$form = document.querySelector('#form');
+    this.$notes = document.querySelector('#notes');
     this.$noteTitle = document.querySelector('#note-title');
     this.$noteText = document.querySelector('#note-text');
     this.$formButtons = document.querySelector('#form-buttons');
@@ -49,6 +51,8 @@ class App {
     this.$form.classList.remove('form-open');
     this.$noteTitle.style.display = 'none';
     this.$formButtons.style.display = 'none';
+    this.$noteTitle.value = '';
+    this.$noteText.value = '';
   }
   // add a note
   addNote(note) {
@@ -60,8 +64,31 @@ class App {
       id: this.notes.length > 0 ? this.notes[this.notes.length -1].id + 1 : 1
     };
     this.notes = [...this.notes, newNote];
-    console.log(this.notes);
+    console.log(this.notes); // debug
+    this.displayNotes();
+    this.closeForm();
   }
+
+  // display notes
+  displayNotes() {
+    const hasNotes = this.notes.length > 0;
+    this.$placeholder.style.display = hasNotes ? 'none' : 'flex';
+
+    this.$notes.innerHTML = this.notes.map(note => `
+      <div style="background: ${note.color};" class="note">
+        <div class="${note.title && 'note-title'}">${note.title}</div> 
+        <div class="note-text">${note.text}</div>
+        <div class="toolbar-container">
+          <div class="toolbar">
+            <img class="toolbar-color" src="https://icon.now.sh/palette">
+            <img class="toolbar-delete" src="https://icon.now.sh/delete">
+          </div>
+        </div>
+      </div>
+    `).join('');
+  }
+
+
 }
 
 new App()
